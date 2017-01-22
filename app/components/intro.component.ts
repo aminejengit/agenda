dda7d80bbc648508b19f840e925b4729a8280ff0
amine.jendoubi <amine.jendoubi@ensi-uma.tn>
2017-01-22 08:49:15 +0100

    add a data binder to switch between the intro views

--- app/components/intro.component.ts
+++ app/components/intro.component.ts
@@ -5,5 +5,11 @@ import { Component } from '@angular/core';
   templateUrl: `/partials/index-intro.html`,
 })
 export class IntroComponent {
-    
+    showSecondView="slider";
+    showSLider():void{
+      this.showSecondView="slider";
+    }
+    showResult():void{
+      this.showSecondView="result";
+    }
 } 
