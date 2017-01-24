022ee11a3b90dbb6a58cf91e48078cda6c9a006e
amine jendoubi <amine.jendoubi@aderivatives.com>
2017-01-24 14:55:37 +0100

    ddd

--- app/app.js
+++ app/app.js
@@ -1,28 +1,49 @@
-var app = angular.module("myApp",[]);
-app.controller("myCtrl", function($scope,dataService) {
+var app = angular.module("myApp", []);
+app.controller("myCtrl", function ($scope, dataService) {
 
     // handle intro section
-    $scope.introSection="slider";
-    $scope.localty="";
-    $scope.showSlider = function(){
-        $scope.introSection="slider";
+    $scope.introSection = "slider";
+    $scope.showSlider = function () {
+        $scope.introSection = "slider";
     }
-$scope.onvalueChange = function(){
-    console.log($scope.local)
-}
+
+    //input validation 
+    $scope.validInput = false;
+    $scope.doctorOrCenter="";
+    $scope.spec="";
+    $scope.local="";
+    $scope.date="";
+    $scope.onvalueChangeDoctorCenter = function () {   
+         if($scope.doctorOrCenter.length>0 || (
+                $scope.spec.length>0 && $scope.local.length>0 && $scope.date.length>0 
+                )){
+            $scope.validInput = true;
+        }else{
+            $scope.validInput = false;
+        }
+        console.log($scope.validInput);
+    }
+
+
+    $scope.showResult = function () {
+        if($scope.validInput){
+            $scope.introSection = "result";
+        }
+        $scope.validInput=false;
+    }
+    
   
     
-    $scope.showResult = function(){
-        console.log($scope.localty+"ffff");      
-            $scope.introSection="result";
-           console.log($scope.introSection+"ffff");
-    }
     //import search results
-    $scope.doctors=dataService.getSearchDoctors();
+    $scope.doctors = dataService.getSearchDoctors();
     // select doctor to show his profile
-    $scope.selectedDoctor={};
-    $scope.showProfile = function(doctor){
+    $scope.selectedDoctor = {};
+    $scope.showProfile = function (doctor) {
         $scope.selectedDoctor = doctor;
-        $scope.introSection="profile";
+        $scope.introSection = "profile";
+    }
+    $scope.closeProfile = function () {
+        $scope.introSection = "result";
+        
     }
 });
